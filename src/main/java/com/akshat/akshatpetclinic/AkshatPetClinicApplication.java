d3ce2d60f410129888702af63342797102a28dee
akshatcr7 <imakshat147@gmail.com>
2019-05-12 15:47:38 +0530

    Initial commit

---
+++ src/main/java/com/akshat/akshatpetclinic/AkshatPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.akshat.akshatpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkshatPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkshatPetClinicApplication.class, args);
+	}
+
+}
